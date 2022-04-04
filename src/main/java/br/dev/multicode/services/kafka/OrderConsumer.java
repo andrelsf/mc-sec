3238e6916b1fcce72db13a28879c97313f2cff9e
Andre Ferreira <andre.souza@sensedia.com>
2022-04-04 16:42:34 -0300

    Refactoring

--- src/main/java/br/dev/multicode/services/kafka/OrderConsumer.java
+++ src/main/java/br/dev/multicode/services/kafka/OrderConsumer.java
@@ -1,8 +1,11 @@
 package br.dev.multicode.services.kafka;
 
 import br.dev.multicode.models.OrderMessage;
+import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata;
+import java.util.concurrent.CompletionStage;
 import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import org.jboss.logging.Logger;
 
 @ApplicationScoped
@@ -11,9 +14,16 @@ public class OrderConsumer {
   private final Logger logger = Logger.getLogger(this.getClass());
 
   @Incoming("sec-new-order")
-  public void receive(OrderMessage orderMessage)
+  public CompletionStage<Void> receive(Message<OrderMessage> orderMessage)
   {
-    logger.infof("Got a order message: %s", orderMessage.getOrderId());
+    var metadata = orderMessage.getMetadata(IncomingKafkaRecordMetadata.class)
+        .orElseThrow();
+
+    OrderMessage orderMessageReceived = orderMessage.getPayload();
+
+    logger.infof("%s - Got a order message: %s", metadata.getTopic(), orderMessageReceived.getOrderId());
+
+    return orderMessage.ack();
   }
 
 }
