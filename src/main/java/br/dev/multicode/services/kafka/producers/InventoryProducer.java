d097e1b584abdcc7fdfa4582eb4e6cba284e6141
Andre Ferreira <andre.souza@sensedia.com>
2022-04-05 09:54:40 -0300

    Producer message to kafka notify inventory

---
+++ src/main/java/br/dev/multicode/services/kafka/producers/InventoryProducer.java
@@ -0,0 +1,49 @@
+package br.dev.multicode.services.kafka.producers;
+
+
+import br.dev.multicode.models.OrderMessage;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import java.util.concurrent.CompletableFuture;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class InventoryProducer {
+
+  private final Logger logger = Logger.getLogger(this.getClass());
+
+  @Inject
+  @Channel("sec-inventory")
+  Emitter<OrderMessage> emitter;
+
+  public void doNotification(OrderMessage orderMessage) {
+    Uni.createFrom()
+        .item(orderMessage)
+        .emitOn(Infrastructure.getDefaultWorkerPool())
+        .subscribe()
+        .with(this::sendOrderToKafka, Throwable::new);
+  }
+
+  private Uni<Void> sendOrderToKafka(final OrderMessage orderMessage)
+  {
+    logger.infof("Start of send message to Kafka topic inventory");
+
+//    emitter.send(Message.of(orderMessage)
+//        .withAck(() -> {
+//          logger.infof("Message sent successfully. eventId=%s", orderMessage.getEventId());
+//          return CompletableFuture.completedFuture(null);
+//        })
+//        .withNack(throwable -> {
+//          logger.infof("Message sent failed. ERROR: %s", throwable.getMessage());
+//          return CompletableFuture.completedFuture(null);
+//        }));
+
+    return Uni.createFrom().voidItem();
+  }
+
+}
