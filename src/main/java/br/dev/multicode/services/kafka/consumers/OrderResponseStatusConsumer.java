0c077e164334001db43ecd426f64f45a1614cc7a
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 20:46:53 -0300

    Implementation service to produce orderStatus

--- src/main/java/br/dev/multicode/services/kafka/consumers/OrderResponseStatusConsumer.java
+++ src/main/java/br/dev/multicode/services/kafka/consumers/OrderResponseStatusConsumer.java
@@ -1,17 +1,23 @@
 package br.dev.multicode.services.kafka.consumers;
 
+import br.dev.multicode.models.CurrentOrderStatus;
 import br.dev.multicode.models.OrderProcessingStatus;
+import br.dev.multicode.services.kafka.producers.OrderStatusProducer;
 import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata;
 import java.util.concurrent.CompletionStage;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 import org.jboss.logging.Logger;
 
 @ApplicationScoped
-public class OrderStatusConsumer {
+public class OrderResponseStatusConsumer {
 
   private final Logger log = Logger.getLogger(this.getClass());
+
+  @Inject
+  OrderStatusProducer orderStatusProducer;
 
   @Incoming("sec-response-status")
   public CompletionStage<Void> receiveOrderStatusResponseFromKafka(Message<OrderProcessingStatus> orderProcessingStatus)
@@ -20,8 +26,11 @@ public class OrderStatusConsumer {
         .orElseThrow();
 
     OrderProcessingStatus orderProcessingStatusReceived = orderProcessingStatus.getPayload();
-    log.infof("SEC: %s - Got a order message: orderId=%s", metadata.getTopic(),
-        orderProcessingStatusReceived.getOrderId());
+    log.infof("SEC: %s - Got a order message: orderId=%s :: Status=%s", metadata.getTopic(),
+        orderProcessingStatusReceived.getOrderId(), orderProcessingStatusReceived.getStatus());
+
+    orderStatusProducer.doNotification(new CurrentOrderStatus(orderProcessingStatusReceived.getOrderId(),
+        orderProcessingStatusReceived.getStatus()));
 
     return orderProcessingStatus.ack();
   }
