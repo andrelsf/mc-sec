c697a779c7065b25eef57b1d0288aad5c2db5b24
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 20:45:00 -0300

    Added producer topic sec-order-status

---
+++ src/main/java/br/dev/multicode/services/kafka/producers/OrderStatusProducer.java
@@ -0,0 +1,49 @@
+package br.dev.multicode.services.kafka.producers;
+
+import br.dev.multicode.models.CurrentOrderStatus;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import java.util.concurrent.CompletableFuture;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class OrderStatusProducer {
+
+  private final Logger logger = Logger.getLogger(this.getClass());
+
+  @Inject
+  @Channel("sec-order-status")
+  Emitter<CurrentOrderStatus> emitter;
+
+  public void doNotification(CurrentOrderStatus currentOrderStatus)
+  {
+    Uni.createFrom()
+        .item(currentOrderStatus)
+        .emitOn(Infrastructure.getDefaultWorkerPool())
+        .subscribe()
+        .with(this::sendToKafka, Throwable::new);
+  }
+
+  private Uni<Void> sendToKafka(final CurrentOrderStatus currentOrderStatus)
+  {
+    logger.infof("Start of send message to kafka topic sec-order-status");
+
+    emitter.send(Message.of(currentOrderStatus)
+      .withAck(() -> {
+        logger.infof("Message sent successfully. orderId=%s", currentOrderStatus.getOrderId());
+        return CompletableFuture.completedFuture(null);
+      })
+      .withNack(throwable -> {
+        logger.errorf("Failed to send the message. ERROR: %s", throwable.getMessage());
+        return CompletableFuture.completedFuture(null);
+      }));
+
+    return Uni.createFrom().voidItem();
+  }
+
+}
