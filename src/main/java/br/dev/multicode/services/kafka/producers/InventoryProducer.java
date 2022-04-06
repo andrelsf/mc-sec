68505ed3da3efabbe8761a83eba5c5240cba1e3e
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 20:40:26 -0300

    Fixed logger as error

--- src/main/java/br/dev/multicode/services/kafka/producers/InventoryProducer.java
+++ src/main/java/br/dev/multicode/services/kafka/producers/InventoryProducer.java
@@ -39,7 +39,7 @@ public class InventoryProducer {
           return CompletableFuture.completedFuture(null);
         })
         .withNack(throwable -> {
-          logger.infof("Message sent failed. ERROR: %s", throwable.getMessage());
+          logger.errorf("Message sent failed. ERROR: %s", throwable.getMessage());
           return CompletableFuture.completedFuture(null);
         }));
 
