db16c6300a84b64c0830ac396140c6faaff64bac
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 21:45:13 -0300

    Fixed persistence service OrderSecService

--- src/main/java/br/dev/multicode/services/kafka/consumers/OrderConsumer.java
+++ src/main/java/br/dev/multicode/services/kafka/consumers/OrderConsumer.java
@@ -1,9 +1,12 @@
-package br.dev.multicode.services.kafka;
+package br.dev.multicode.services.kafka.consumers;
 
 import br.dev.multicode.models.OrderMessage;
+import br.dev.multicode.services.OrderSecEventService;
+import br.dev.multicode.services.kafka.producers.InventoryProducer;
 import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata;
 import java.util.concurrent.CompletionStage;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 import org.jboss.logging.Logger;
@@ -13,6 +16,10 @@ public class OrderConsumer {
 
   private final Logger logger = Logger.getLogger(this.getClass());
 
+  @Inject InventoryProducer inventoryProducer;
+
+  @Inject OrderSecEventService orderSecEventService;
+
   @Incoming("sec-new-order")
   public CompletionStage<Void> receive(Message<OrderMessage> orderMessage)
   {
@@ -20,8 +27,10 @@ public class OrderConsumer {
         .orElseThrow();
 
     OrderMessage orderMessageReceived = orderMessage.getPayload();
-
     logger.infof("%s - Got a order message: %s", metadata.getTopic(), orderMessageReceived.getOrderId());
+
+    inventoryProducer.doNotification(orderMessageReceived);
+    orderSecEventService.create(orderMessageReceived);
 
     return orderMessage.ack();
   }
