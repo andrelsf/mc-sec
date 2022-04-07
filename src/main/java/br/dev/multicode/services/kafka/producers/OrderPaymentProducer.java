3c734ec534883ccd5e4a2ab43af3e8a474851275
Andre Ferreira <andre.souza@sensedia.com>
2022-04-07 09:17:18 -0300

    Added and defined config the outgoing channel to notify payment service

---
+++ src/main/java/br/dev/multicode/services/kafka/producers/OrderPaymentProducer.java
@@ -0,0 +1,48 @@
+package br.dev.multicode.services.kafka.producers;
+
+import br.dev.multicode.models.OrderPaymentMessage;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import java.util.concurrent.CompletableFuture;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class OrderPaymentProducer {
+
+  private final Logger logger = Logger.getLogger(this.getClass());
+
+  @Inject
+  @Channel("sec-payment")
+  Emitter<OrderPaymentMessage> emitter;
+
+  public void doNotification(OrderPaymentMessage orderPaymentMessage)
+  {
+    Uni.createFrom()
+        .item(orderPaymentMessage)
+        .emitOn(Infrastructure.getDefaultWorkerPool())
+        .subscribe()
+        .with(this::sendOrderPaymentToKafka, Throwable::getMessage);
+  }
+
+  private Uni<Void> sendOrderPaymentToKafka(final OrderPaymentMessage orderPaymentMessage)
+  {
+    logger.infof("Start of sending the payment message. orderId=%s", orderPaymentMessage.getOrderId());
+
+    emitter.send(Message.of(orderPaymentMessage)
+        .withAck(() -> {
+          logger.infof("Message sent successfully. orderId=%s", orderPaymentMessage.getOrderId());
+          return CompletableFuture.completedFuture(null);
+        })
+        .withNack(throwable -> {
+          logger.errorf("Failed to send message. ERROR: %s", throwable.getMessage());
+          return CompletableFuture.completedFuture(null);
+        }));
+
+    return Uni.createFrom().voidItem();
+  }
+}
