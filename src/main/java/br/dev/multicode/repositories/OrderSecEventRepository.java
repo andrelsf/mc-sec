6e3927ea9c6c4b7ec3d5e8f9586bb5c5ad7167cd
Andre Ferreira <andre.souza@sensedia.com>
2022-04-07 09:23:44 -0300

    Implementation method to update status

--- src/main/java/br/dev/multicode/repositories/OrderSecEventRepository.java
+++ src/main/java/br/dev/multicode/repositories/OrderSecEventRepository.java
@@ -1,9 +1,13 @@
 package br.dev.multicode.repositories;
 
 import br.dev.multicode.entities.OrderSecEvent;
+import br.dev.multicode.enums.OrderStatus;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
+import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
+import javax.ws.rs.NotFoundException;
 
 @ApplicationScoped
 public class OrderSecEventRepository implements PanacheRepository<OrderSecEvent> {
@@ -14,4 +18,16 @@ public class OrderSecEventRepository implements PanacheRepository<OrderSecEvent>
     this.persistAndFlush(orderSecEvent);
   }
 
+  @Transactional
+  public OrderSecEvent updateStatusBy(final UUID orderId, final OrderStatus status)
+  {
+    OrderSecEvent orderSecEvent = this.find("order_id = :orderId",
+            Parameters.with("orderId", orderId.toString()))
+        .firstResultOptional()
+        .orElseThrow(() ->
+            new NotFoundException("Order not found by id=".concat(orderId.toString())));
+
+    orderSecEvent.setStatus(status);
+    return orderSecEvent;
+  }
 }
