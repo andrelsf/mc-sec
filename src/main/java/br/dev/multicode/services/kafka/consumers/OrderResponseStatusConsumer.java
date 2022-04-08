fb80f66c01c3beec8ee6ba9297df8ac47f03d5df
Andre Ferreira <andre.souza@sensedia.com>
2022-04-08 07:54:52 -0300

    Refactoring and hot fixed with method blocking

--- src/main/java/br/dev/multicode/services/kafka/consumers/OrderResponseStatusConsumer.java
+++ src/main/java/br/dev/multicode/services/kafka/consumers/OrderResponseStatusConsumer.java
@@ -4,9 +4,9 @@ import br.dev.multicode.entities.OrderSecEvent;
 import br.dev.multicode.enums.OrderStatus;
 import br.dev.multicode.models.CurrentOrderStatus;
 import br.dev.multicode.models.OrderProcessingStatus;
+import br.dev.multicode.services.NotificationService;
 import br.dev.multicode.services.OrderSecEventService;
-import br.dev.multicode.services.kafka.producers.OrderPaymentProducer;
-import br.dev.multicode.services.kafka.producers.OrderStatusProducer;
+import io.smallrye.reactive.messaging.annotations.Blocking;
 import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata;
 import java.util.concurrent.CompletionStage;
 import javax.enterprise.context.ApplicationScoped;
@@ -20,10 +20,10 @@ public class OrderResponseStatusConsumer {
 
   private final Logger log = Logger.getLogger(this.getClass());
 
-  @Inject OrderStatusProducer orderStatusProducer;
-  @Inject OrderPaymentProducer orderPaymentProducer;
+  @Inject NotificationService notificationService;
   @Inject OrderSecEventService orderSecEventService;
 
+  @Blocking
   @Incoming("sec-response-status")
   public CompletionStage<Void> receiveOrderStatusResponseFromKafka(Message<OrderProcessingStatus> orderProcessingStatus)
   {
@@ -37,11 +37,10 @@ public class OrderResponseStatusConsumer {
     final CurrentOrderStatus currentOrderStatus = new CurrentOrderStatus(
         orderProcessingStatusReceived.getOrderId(), orderProcessingStatusReceived.getStatus());
 
-    orderStatusProducer.doNotification(currentOrderStatus);
-
+    notificationService.doNotificationOrderService(currentOrderStatus);
     final OrderSecEvent orderSecEvent = orderSecEventService.updateStatus(currentOrderStatus);
     if (orderProcessingStatusReceived.getStatus().equals(OrderStatus.RESERVED_PRODUCTS)) {
-      orderPaymentProducer.doNotification(orderSecEvent.toOrderPaymentMessage());
+      notificationService.doNotificationOrderPayment(orderSecEvent.toOrderPaymentMessage());
     }
 
     return orderProcessingStatus.ack();
